1a6f5a2945ffd79b5bf8d1855815fe7446786b2e
Alexander Sidorin <a.d.sidorin@gmail.com>
2021-09-23 11:27:48 +0300

    feat: Category implemented in store

--- store/todo-store.ts
+++ store/todo-store.ts
@@ -1,71 +1,22 @@
 import { makeAutoObservable } from "mobx";
-import { TodoType } from "../types";
+import { Todo, Category } from "../types";
 
 export class TodoStore {
-  todos: TodoType[] = [
-    /*
-    {
-      id: 1,
-      text: 'To do todo',
-      completed: false,
-      date: new Date().toLocaleString(),
-    },
-    {
-      id: 2,
-      text: 'And learn React Native',
-      completed: false,
-      date: new Date().toLocaleString(),
-    },
-    {
-      id: 3,
-      text: 'And have fun =)',
-      completed: false,
-      date: new Date().toLocaleString(),
-    },
-    {
-      id: 4,
-      text: 'Check todos with very long long long text, its important, like, really really important',
-      completed: false,
-      date: new Date().toLocaleString(),
-    },
-    
-    {
-      id: 5,
-      text: 'And have fun =)',
-      completed: false,
-      date: new Date().toLocaleString(),
-    },
-    {
-      id: 6,
-      text: 'And have fun =)',
-      completed: false,
-      date: new Date().toLocaleString(),
-    },
-    {
-      id: 7,
-      text: 'And have fun =)',
-      completed: false,
-      date: new Date().toLocaleString(),
-    },
-    {
-      id: 8,
-      text: 'And have fun =)',
-      completed: false,
-      date: new Date().toLocaleString(),
-    },
-    */
-  ];
+  todos: Todo[] = [];
+
+  categories: Category[] = []
 
   constructor() {
     makeAutoObservable(this);
   }
 
-  addTodo(newTodoText: string) {
+  addTodo(newTodoText: string, categoryId: number) {
     const newTodo = {
       id: this.todos.length + 1,
       text: newTodoText,
       completed: false,
       date: new Date().toLocaleString(),
+      categoryId,
     };
 
     this.todos.push(newTodo);
@@ -86,5 +37,4 @@ export class TodoStore {
   removeTodo(id: number) {
     this.todos = this.todos.filter((todo) => todo.id !== id);
   }
-
 }
